cfb57ae001dcafd38aa7cf0bf011253ed6058c97
firas najjar <162709181+firas988@users.noreply.github.com>
2024-07-10 22:00:42 +0300

    more change
    more change

--- javascript/index_js.js
+++ javascript/index_js.js
@@ -1,7 +1,9 @@
+"use strict";
+
 const image = "https://picsum.photos/200/300?random=";
 let users = [
   {
-    name: "Mohammmad",
+    name: "Mohammad",
     number: "0525290963",
     email: "Mohammmad@example.com",
     address: "ba8h",
@@ -23,11 +25,21 @@ let users = [
     address: "mdre",
     description: "qq",
     img: "https://picsum.photos/200/300?random="
-  }
+  },
+  {
+    name: "mozan",
+    number: "0525298981",
+    email: "mozan@example.com",
+    address: "moo",
+    description: "hh",
+    img: "https://picsum.photos/200/300?random="
+  },
+
 ];
 
 function showUsers() {
   let user = "";
+  users.sort((a, b) => a.name.localeCompare(b.name));
   users.forEach(function (person, index) {
     user += `<li class="frame">
           <div class="User">
@@ -43,7 +55,7 @@ function showUsers() {
               <button onClick="open_popup('more_user_info');display_user(${index})" class="info_settings" ><img src="Images/user_info.png" width="40em" alt="showinfo"
                   title="more  info"></button>
 
-              <button onClick="change_value(${index});open_popup('edit_user_info');show_user(${index})" class="info_settings" ><img src="Images/user_edit.png" width="40em" alt="user_edit"
+              <button onClick="change_value(${index});open_popup('Add_user');show_user(${index})" class="info_settings" ><img src="Images/user_edit.png" width="40em" alt="user_edit"
                   title="user edit"></button>
 
               <button onclick="open_popup('ask_delete');change_value(${index})" class="info_settings" ><img src="Images/delet.png" width="40em"
@@ -54,11 +66,60 @@ function showUsers() {
 
         </li>`
   })
-
   document.getElementById("users").innerHTML = user;
-  //UPDATES CONTACT COUNTER
-  document.getElementById("counter_users").innerHTML = `<h2>${users.length} Contacts</h2>`;
+  updatecount(users.length)
+
 }
+
+function updatecount(value) {
+  //UPDATES CONTACT COUNTER
+  document.getElementById("counter_users").innerHTML = `<h2>${value} Contacts</h2>`;
+}
+
+
+
+function has(name, text) {
+  if (text.length > name.length)
+    return false;
+  else
+    for (let i = 0; i < text.length; i++)
+      if (name[i].toUpperCase() !== text[i].toUpperCase() && name[i].toLowerCase() !== text[i].toLowerCase())
+        return false
+
+  return true;
+}
+function search() {
+
+
+  let allcontacts = document.getElementsByClassName("frame");
+
+  let searchvalue = document.getElementById("Search").value
+  let searchtext = document.createElement('span')
+  searchtext.textContent = searchvalue;
+
+
+  if (event.target === document.getElementById("Search")) {
+    users.forEach((e, i) => {
+
+      let name = allcontacts[i].querySelector('.user_name').textContent
+
+      if (has(name, searchtext.textContent.trim())) {
+        allcontacts[i].classList.add("show")
+        allcontacts[i].classList.remove("hide")
+      }
+      else if (!has(name, searchtext.textContent.trim())) {
+        allcontacts[i].classList.remove("show")
+        allcontacts[i].classList.add("hide")
+      }
+    })
+
+  }
+
+  updatecount(document.getElementsByClassName('show').length)
+
+
+}
+
 
 function open_popup(x) {
   document.getElementById("popup").style.display = 'flex';
@@ -67,21 +128,170 @@ function open_popup(x) {
 }
 
 function close_popup(event) {
-  if (event.target == document.getElementById("popup") || event == 'close') {
+  if (event.target == document.getElementById("popup") || event == 'close' || event.keyCode == 27 && document.getElementById("popup").style.display == 'flex') {
     document.getElementById("popup").style.display = 'none';
     document.querySelector(".show").classList.remove("show");
+    document.getElementById('name').value = "";
+    document.getElementById('number').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('address').value = "";
+    document.getElementById('description').value = "";
+    document.getElementById("user_img_add").src = image;
+    document.getElementById("add_button").innerHTML = ` <button onclick="close_popup('close')" id="cancel_add">Cancel</button>
+            <button onclick="add_user()" id="save_add" >Add</button>`;
+
+    document.getElementById("contact_email").style.display = "block";
+    document.getElementById("contact_address").style.display = "block";
+    document.getElementById("contact_description").style.display = "block";
+
+    document.getElementById('name').classList.remove("redborder");
+    document.getElementById('number').classList.remove("redborder");
+    document.getElementById('email').classList.remove("redborder");
+    document.querySelectorAll(".error_msg").forEach(elem =>
+      elem != null ? elem.remove() : console.log()
+    )
   }
+
+
+
 
 }
 
-function add_user() {
-  //ADD USER
-  let x = document.getElementById('number').value.toString();
-  let y = document.getElementById('name').value;
-  if (x.length != 10 || y.length == 0) {
-    alert("Invalid number or name");
+function name_check(index) {
+
+
+  let error = document.createElement("p");
+  error.classList.add('error_msg');
+  error.classList.add('hide');
+
+  let x = document.getElementById('name');
+  x.after(error);
+
+  if (x.value.length == 0) {
+    x.classList.add("redborder");
+    error.textContent = "Can't have an empty name field.";
+    error.classList.remove('hide');
+
+    return false;
   }
-  else {
+
+
+  for (let i = 0; i < users.length; i++) {
+    if (x.value.trim() == users[i].name.trim() && index != i) {
+      x.classList.add("redborder");
+
+      document.querySelector('#Name_label .error_msg').textContent = "Name already exists in contacts.";
+      error.classList.remove('hide');
+
+      return false;
+    }
+  }
+  x.classList.remove("redborder");
+  document.querySelector("#Name_label .error_msg").remove();
+  return true;
+}
+
+function number_check(index) {
+  let error = document.createElement("p");
+  error.classList.add('error_msg');
+  error.classList.add('hide');
+
+
+  let x = document.getElementById('number');
+  let stringX = x.value.toString();
+  x.after(error)
+
+  if (stringX.length != 10) {
+    x.classList.add("redborder");
+    error.innerHTML = `Phone number must <br/> have exactly 10 numbers.`;
+    error.classList.remove('hide');
+
+    return false;
+  }
+
+  for (let i = 0; i < stringX.length; i++) {
+    if (stringX[i] < '0' || stringX[i] > '9') {
+      x.classList.add("redborder");
+      return false;
+    }
+  }
+
+  for (let i = 0; i < users.length; i++) {
+    if (stringX.trim() == users[i].number.trim() && index != i) {
+      x.classList.add("redborder");
+      error.innerHTML = `Phone number is already in contacts.`;
+      error.classList.remove('hide');
+
+      return false;
+    }
+  }
+
+  x.classList.remove("redborder");
+  document.querySelector("#Number_label .error_msg").remove();
+
+  return true;
+}
+function email_check(index) {
+  let error = document.createElement("p");
+  error.classList.add('error_msg');
+  error.classList.add('hide');
+
+  let x = document.getElementById("email");
+  x.after(error);
+
+
+  if (x.value.toString().length == 0) {
+    return true;
+  }
+  if (!x.value.toString().includes('@')) {
+    error.innerHTML = "the email has to include (@)";
+    error.classList.remove('hide');
+    x.classList.add("redborder");
+    return false;
+  }
+  let str = x.value.toString().substring(x.value.indexOf('@'));
+  if (!str.includes(".com") && !str.includes(".net")) {
+
+    error.innerHTML = "the email has to <br> include (.com,.net etc..)";
+    error.classList.remove('hide');
+
+    x.classList.add("redborder");
+    return false;
+  }
+
+  for (let i = 0; i < users.length; i++) {
+    if (x.value.toString().trim() == users[i].email.trim() && index != i) {
+      x.classList.add("redborder");
+      alert("email already exists in contacts.");
+      return false;
+    }
+  }
+
+  x.classList.remove("redborder");
+  document.querySelector("#Email_label .error_msg").remove();
+
+  return true;
+}
+
+function add_user() {
+
+  if (document.querySelector('#Name_label .error_msg') !== null)
+    document.querySelector('#Name_label .error_msg').remove();
+
+  if (document.querySelector('#Number_label .error_msg') !== null)
+    document.querySelector('#Number_label .error_msg').remove();
+
+  if (document.querySelector('#Email_label .error_msg') !== null)
+    document.querySelector('#Email_label .error_msg').remove();
+
+
+
+  //ADD USER
+  let name = name_check();
+  let number = number_check();
+  let email = email_check();
+
+  if (name && number && email) {
     users.push({
       name: document.getElementById('name').value,
       number: document.getElementById('number').value,
@@ -103,12 +313,12 @@ function add_user() {
 
 function show_user(index) {
   //DISPLAY USER INFO IN EDIT FORM
-  document.getElementById("pfp").src = users[index].img + index;
-  document.getElementById("edit_name").value = users[index].name;
-  document.getElementById("edit_phone_num").value = users[index].number;
-  document.getElementById("edit_email").value = users[index].email;
-  document.getElementById("edit_address").value = users[index].address;
-  document.getElementById("edit_description").value = users[index].description;
+  document.getElementById("user_img_add").src = users[index].img + index;
+  document.getElementById("name").value = users[index].name;
+  document.getElementById("number").value = users[index].number;
+  document.getElementById("email").value = users[index].email;
+  document.getElementById("address").value = users[index].address;
+  document.getElementById("description").value = users[index].description;
 }
 
 function display_user(index) {
@@ -116,35 +326,59 @@ function display_user(index) {
   document.getElementById("contact_img").src = users[index].img + index;
   document.getElementById("contact_name").textContent = users[index].name;
   document.getElementById("contact_phone").textContent = users[index].number;
-  document.getElementById("phone_email").innerHTML = `
-          <div>
-            <p><span>Email: ${users[index].email}</span> </p>
-          </div>
+  if (users[index].email.length > 0)
+    document.querySelector("#contact_email span").textContent = `Email: ${users[index].email}`;
+  else
+    document.getElementById("contact_email").style.display = "none";
 
-          <div>
-            <p><span>Address: ${users[index].address}</span> </p>
-          </div>
+  if (users[index].address.length > 0)
+    document.querySelector("#contact_address span").textContent = `Address: ${users[index].address}`;
+  else
+    document.getElementById("contact_address").style.display = "none";
 
-          <div>
-            <p><span>Description: ${users[index].description}</span> </p>
-          </div>`;
+  if (users[index].description.length > 0)
+    document.querySelector("#contact_description span").innerHTML = `Description: ${users[index].description}`
+  else
+    document.getElementById("contact_description").style.display = "none";
+
 }
 
 function save_edit() {
   //SAVES EDIT
-  let ind = document.getElementById('save_edit').value;
 
-  if (document.getElementById("edit_phone_num").value.length != 10 || document.getElementById("edit_name").value === "")
-    alert("Invalid name or number")
-  else {
-    users[ind].name = document.getElementById("edit_name").value;
-    users[ind].number = document.getElementById("edit_phone_num").value;
-    users[ind].email = document.getElementById("edit_email").value;
-    users[ind].address = document.getElementById("edit_address").value;
-    users[ind].description = document.getElementById("edit_description").value;
+  if (document.querySelector('#Name_label .error_msg') !== null)
+    document.querySelector('#Name_label .error_msg').remove();
+
+  if (document.querySelector('#Number_label .error_msg') !== null)
+    document.querySelector('#Number_label .error_msg').remove();
+
+  if (document.querySelector('#Email_label .error_msg') !== null)
+    document.querySelector('#Email_label .error_msg').remove();
+
+  let ind = document.getElementById('save_add').value;
+
+  let name = name_check(ind);
+  let number = number_check(ind);
+  let email = email_check(ind);
+
+  if (name && number && email) {
+    users[ind].name = document.getElementById("name").value;
+    users[ind].number = document.getElementById("number").value;
+    users[ind].email = document.getElementById("email").value;
+    users[ind].address = document.getElementById("address").value;
+    users[ind].description = document.getElementById("description").value;
     showUsers();
-
     close_popup('close');
+
+    document.getElementById('name').value = "";
+    document.getElementById('number').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('address').value = "";
+    document.getElementById('description').value = "";
+
+    document.getElementById("add_button").innerHTML = ` <button onclick="close_popup('close')" id="cancel_add">Cancel</button>
+            <button onclick="add_user()" id="save_add" >Add</button>`
+
   }
 
 }
@@ -152,7 +386,11 @@ function save_edit() {
 function change_value(index) {
   //GETS VALUE OF INDEX OF CONTACT TO EDIT/DELETE
   document.getElementById("confirm").value = index;
-  document.getElementById("save_edit").value = index;
+
+  document.getElementById("add_button").innerHTML = ` <button onclick="close_popup('close')" id="cancel_add">Cancel</button>
+            <button onclick="save_edit()" id="save_add" >Save</button>`
+  document.getElementById("save_add").value = index;
+
 }
 
 function delet_user() {
@@ -174,8 +412,6 @@ function delet_user() {
       close_popup('close');
       showUsers();
       conf.checked = false;
-
-
     }
 
   }
